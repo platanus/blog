1e6c0db372a15476cef2fb337f23e44ae17dcaea
Juan Ignacio Donoso <jidonoso@gmail.com>
2015-04-21 15:06:07 -0300

    feat(): update template

--- Gemfile
+++ Gemfile
@@ -2,6 +2,11 @@
 source "https://rubygems.org"
 
 gem "jekyll", "~> 2.5.0"
+gem 'jekyll-assets'
+gem 'rake'
+gem 'sass'
+gem 'uglifier'
+# gem 'github-pages'
 gem "rouge", "~> 1.7.4"
 
 group :jekyll_plugins do
---
+++ _plugins/team_member.rb
@@ -0,0 +1,20 @@
+module Jekyll
+  module TeamMemberFilter
+
+    # Add our new liquid filter.
+    def team_member(authors)
+      return [] if authors.nil?
+
+      authors = authors.map do |github_username|
+        filePath = File.join('_data', 'team', "#{github_username}.yml")
+        data     = SafeYAML.load_file(filePath)
+        data
+      end
+
+      authors
+    end
+
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::TeamMemberFilter)
